9a259159c293dcec34ab6d31b707ef27d08e76c1
Erubsh <rzfly@qq.com>
2021-12-01 23:32:09 +0800

    first commit

---
+++ rtl/control.v
@@ -0,0 +1,15 @@
+
+module control(
+	input [`DATA_WIDTH]instruction,
+	output write_reg,
+	output ALU_src,
+	output [`ALU_OP_WIDTH]ALU_op,
+	output mem2reg,
+	output read_mem,
+	output write_mem,
+
+);
+
+	
+
+endmodule
